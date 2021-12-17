0669d86c94ce657d882a1387ebeabfb7d97bfc8b
MrCadawas <alfred.cadawas@gmail.com>
2021-12-17 09:33:08 -0500

    Added two more print messages

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello Github");
+		System.out.println("Maligayan Pasko");
+		System.out.println("Joyeux Noel");
 	}
 }
